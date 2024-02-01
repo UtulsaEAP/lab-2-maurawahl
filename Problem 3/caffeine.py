1b9d30c9eb5acf1f259cd00162fd740c5faadb0d
maurawahl <mjw4048@utulsa.edu>
2024-02-01 14:43:34 -0600

    problem 3 done

--- Problem 3/caffeine.py
+++ Problem 3/caffeine.py
@@ -1,7 +1,12 @@
 
 def caffeine():
+
+    print("How much caffeine?")
     caffeine_mg = float(input())
-    ''' Type your code here. '''
     
+    print("After 6 hours: " + f'{caffeine_mg/2:.2f}' + " mg")
+    print("After 12 hours: " + f'{caffeine_mg/4:.2f}' + " mg")
+    print("After 24 hours: " + f'{caffeine_mg/16:.2f}' + " mg")
+
 if __name__ == "__main__":
     caffeine()

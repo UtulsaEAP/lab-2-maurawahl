93fba4443adec3a1e24539c7d5131800a4bd3046
maurawahl <mjw4048@utulsa.edu>
2024-02-01 14:07:32 -0600

    problem 1 done

--- Problem 1/right_arrow.py
+++ Problem 1/right_arrow.py
@@ -1,15 +1,21 @@
 def right_arrow():
+
+    print("What is the first character?")
     base_char = input()
+
+    print("What is the second character?")
     head_char = input()
 
     row1 = '      ' + head_char
-    ''' Type your code here. '''
+    row2 = base_char + base_char + base_char + base_char + base_char + base_char + head_char + head_char
+    row3 = base_char + base_char + base_char + base_char + base_char + base_char + head_char + head_char + head_char
+    row4 = base_char + base_char + base_char + base_char + base_char + base_char + head_char
 
     print(row1)
     print(row2)
     print(row3)
     print(row2)
-    print(row1)
+    print(row4)
 
 if __name__ == "__main__":
     right_arrow()

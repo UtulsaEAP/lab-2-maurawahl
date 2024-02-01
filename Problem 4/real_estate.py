3f058364362325f00357c0e11e6bb0d66d024037
maurawahl <mjw4048@utulsa.edu>
2024-02-01 14:49:56 -0600

    problem 4 done

--- Problem 4/real_estate.py
+++ Problem 4/real_estate.py
@@ -1,8 +1,18 @@
 
 def real_estate():
     
+    print("What is the current price?")
     current_price = int(input())
+
+    print("What was last month's price?")
     last_month_price = int(input())
-    # Your code goes here
+
+    price_change = current_price - last_month_price
+
+    mortgage  = (current_price * 0.051) / 12
+
+    print("This house is $" + str(current_price) + ". The change is $" + str(price_change) + " since last month.")
+    print("The estimated monthly mortgage is $" + f'{mortgage:.2f}' + ".")
+
 if __name__ == "__main__":
     real_estate()

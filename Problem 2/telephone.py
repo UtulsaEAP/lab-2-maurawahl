e51e0ec9b28125e33096b9486c35b3b8a651dea0
maurawahl <mjw4048@utulsa.edu>
2024-02-01 14:38:12 -0600

    problem 2 done

--- Problem 2/telephone.py
+++ Problem 2/telephone.py
@@ -1,5 +1,14 @@
 def telephone():
+
+    print("What is your phone number?")
     phone_number = int(input())
-    ''' Type your code here. '''
+
+    first = phone_number // 10000000
+    second = (phone_number % (first*100000)) // 10000
+    third = phone_number % ((phone_number // 10000))
+
+    output = ("(" + str(first) + ")" + " " + str(second) + "-" + str(third))
+    print(output)
+
 if __name__ == "__main__":
     telephone()
